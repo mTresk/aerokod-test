35c8cc238a9d5fb25c02d6f7372e98fe846a3c26
Maxim Tresk <djtresk@gmail.com>
2024-04-19 09:55:36 +0500

    feat: models, migrations, seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Random\RandomException;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * @throws RandomException
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)
+            ->has(Post::factory(random_int(2, 4))
+                ->has(Comment::factory(random_int(2, 3))))
+            ->create();
     }
 }
